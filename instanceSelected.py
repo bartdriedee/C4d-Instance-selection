277bb5fb9b401a3033157c3d44b6b5458186b7fd
bartdriedee <57003626+bartdriedee@users.noreply.github.com>
2020-01-23 19:33:46 +0100

    Update instanceSelected.py

--- instanceSelected.py
+++ instanceSelected.py
@@ -1,34 +1,22 @@
 import c4d
 
 def main():
-    def tool():
-        return plugins.FindPlugin(doc.GetAction(), c4d.PLUGINTYPE_TOOL)
+    doc.StartUndo()
+    objects = doc.GetActiveObjects(0)
+    for obj in objects:
+        print "instancing ",obj.GetName()
+        instance = c4d.BaseObject(c4d.Oinstance)           # create a new instance object
+        instance.SetName(obj.GetName() + '_instance')      # set it's name to the object's name + "_instance"
+        instance.SetRelPos(obj.GetRelPos())         # set it's Relative Position to the object's Relative Position
+        instance.SetRelScale(obj.GetRelScale())     # set it's Relative Scale to the object's Relative Scale
+        instance.SetRelRot(obj.GetRelRot())
+        instance[c4d.INSTANCEOBJECT_LINK] = obj            # set the instance's source object
+        instance.InsertBefore(obj)                         # insert the instance object in the object manager,
+                                                           # right before the source object. You can use other options.
+                                                           # Like .InsertAfter(obj), if you want it after the source object.
+        doc.AddUndo(c4d.UNDOTYPE_NEW, instance)
+    doc.EndUndo()
 
-    def object(i=0):
-        return doc.GetActiveObjects(c4d.GETACTIVEOBJECTFLAGS_CHILDREN | c4d.GETACTIVEOBJECTFLAGS_SELECTIONORDER)
-
-    def tag():
-        return doc.GetActiveTag()
-
-    def renderdata():
-        return doc.GetActiveRenderData()
-
-    def prefs(id):
-        return plugins.FindPlugin(id, c4d.PLUGINTYPE_PREFS)
-    
-    def clearSelection(selection):
-        for i in xrange(len(selection)):
-            selection[i].DelBit(c4d.BIT_ACTIVE)
-    
-    selection = object()
-    for i in xrange(len(selection)):
-        clearSelection(object())
-        doc.AddUndo(c4d.UNDOTYPE_CHANGE_SMALL, selection[i])
-        print ("Making duplicate of object: {}".format(selection[i][c4d.ID_BASELIST_NAME]))
-        selection[i].SetBit(c4d.BIT_ACTIVE)
-        c4d.CallCommand(5126) # Instance
-        
-        
 if __name__ == '__main__':
     main()
     c4d.EventAdd()
